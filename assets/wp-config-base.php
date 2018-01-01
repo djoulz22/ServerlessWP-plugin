dd02ac9d8c17aab10ae54ea56e11b3edcdf3439a
Mitch MacKenzie <mitchmac@gmail.com>
2018-01-01 12:21:17 -0400

    Update wp-config-base.php

--- assets/wp-config-base.php
+++ assets/wp-config-base.php
@@ -46,7 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-// salt-replace
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
 
 /**
  * WordPress Database Table prefix.
