5a894f5a18828a650dbe86ec3a2246c4f7b58517
Mitch MacKenzie <mitchmac@gmail.com>
2018-01-04 19:36:08 -0400

    Remove unneeded links for a static website

--- serverlesswp.php
+++ serverlesswp.php
@@ -10,6 +10,24 @@ License: GPLv2 or later
 Text Domain: serverlesswp
 */
 
+add_filter( 'xmlrpc_enabled', '__return_false' );
+add_filter( 'feed_links_show_comments_feed', '__return_false' );
+
+add_action( 'init', 'serverlesswp_cleanup' );
+
+function serverlesswp_cleanup() {
+	remove_action( 'wp_head', 'rsd_link' );
+	remove_action( 'wp_head', 'wlwmanifest_link' );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10);
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10);
+	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0);
+	remove_action( 'wp_head', 'feed_links_extra', 3 );
+	remove_action( 'wp_head', 'feed_links', 2 );
+	remove_action( 'wp_head', 'rel_canonical' );
+}
+
 add_filter( 'aws_get_client_args', 'serverlesswp_filter_aws_args' );
 
 function serverlesswp_filter_aws_args($args) {
